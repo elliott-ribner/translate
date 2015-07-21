dc4f51f3530b08a63fdaf8f7ec3710639f7e27da
elliott-ribner <elr886@gmail.com>
2015-07-21 14:29:05 -0400

    replacing original

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -2,11 +2,22 @@ class Phrase
   include ActiveModel::Model
   
   TRANSLATORS = %w{ normal no-vowels reversed chicken }
-
+ 
   attr_accessor :original_text, :translator
-
+ 
   def translated_text
-    original_text
   end
-
+ 
+ 
+  def remove_vowels
+    
+  end
+ 
+  def reversed
+    
+  end
+ 
+  def chicken
+    
+  end
 end
